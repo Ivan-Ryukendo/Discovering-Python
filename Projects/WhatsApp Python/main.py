ef84f6a1e6bcfdf1e6a3633efcd4cd657759470d
Mirza Gamal Abdel Nasser <gamal.nasser.gfx@gmail.com>
2024-09-01 20:20:14 +0600

    Removed

--- Projects/WhatsApp Python/main.py
+++ Projects/WhatsApp Python/main.py
@@ -1,5 +1,5 @@
 import pywhatkit as wakit
 
-wakit.sendwhatmsg("+8801992181113", "ami ar message kortam ei na shadule message koratam python dia\nhahahahhahah", 2, 30)
+wakit.sendwhatmsg("number", "ami ar message kortam ei na shadule message koratam python dia\nhahahahhahah", 2, 30)
 
 # wakit.image_to_ascii_art("packet-cake.png", "packet-cake-ascii-art")
