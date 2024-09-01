6a6c6eff2ea672cab9a8f0323a9e0b50e44d3347
Mirza Gamal Abdel Nasser <gamal.nasser.gfx@gmail.com>
2024-09-01 20:21:33 +0600

    Update main.py

--- Projects/WhatsApp Python/main.py
+++ Projects/WhatsApp Python/main.py
@@ -2,4 +2,4 @@ import pywhatkit as wakit
 
 wakit.sendwhatmsg("number", "ami ar message kortam ei na shadule message koratam python dia\nhahahahhahah", 2, 30)
 
-# wakit.image_to_ascii_art("packet-cake.png", "packet-cake-ascii-art")
+# wakit.image_to_ascii_art("img.png", "imgascii-art")
